eb524415f79efadcb087f8f620762f51af2d0121
Fernando Vargas <nandovargas7@gmail.com>
2023-01-29 14:18:16 -0300

    Correcao de todos os erros do lint

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ doxy.parse_doxyfile('Doxyfile')
 doxy.run()
 
 if __name__ != "__main__":
-    exit()   
+    exit()
+
 
 def get_config():
     """
@@ -25,18 +26,19 @@ def get_config():
     for file in os.listdir('src/yamls'):
         filepath = os.path.join('src/yamls', file)
         config = initialParser(filepath).parse()
-    
+
         features_configs, columns_set_alias = featureEngineringParser(filepath).parse(config['feature_engineering'])
         del config['feature_engineering']
-        
+
         model_configs = modelParser(columns_set_alias).parse(config['model'])
         del config['model']
-        
+
         print("FEATURES")
         print(features_configs)
         print(3*'\n')
         print(20*'-')
         print(3*'\n')
         print(model_configs)
-         
+
+
 get_config()
